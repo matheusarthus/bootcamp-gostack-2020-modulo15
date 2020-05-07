59c7235beb839642f332a9ae3403018ac57ae012
Matheus Alves Arthus <matheus.arthus@gmail.com>
2020-05-07 14:19:22 -0300

    Abstraindo Validações

--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,9 @@ import NotificationController from './app/controllers/NotificationController';
 import AvailableController from './app/controllers/AvailableController';
 
 import validateUserStore from './app/validators/UserStore';
+import validateUserUpdate from './app/validators/UserUpdate';
+import validateAppointmentStore from './app/validators/AppointmentStore';
+import validateSessionStore from './app/validators/SessionStore';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -20,16 +23,20 @@ const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post('/users', validateUserStore, UserController.store);
-routes.post('/sessions', SessionController.store);
+routes.post('/sessions', validateSessionStore, SessionController.store);
 
 routes.use(authMiddleware);
 
-routes.put('/users', UserController.update);
+routes.put('/users', validateUserUpdate, UserController.update);
 
 routes.get('/providers', ProviderController.index);
 routes.get('/providers/:providerId/available', AvailableController.index);
 
-routes.post('/appointments', AppointmentController.store);
+routes.post(
+  '/appointments',
+  validateAppointmentStore,
+  AppointmentController.store
+);
 routes.get('/appointments', AppointmentController.index);
 routes.delete('/appointments/:id', AppointmentController.delete);
 
